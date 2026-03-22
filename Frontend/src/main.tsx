df13231cc39a4fb2c9edc31d608e7535a994eff3
Bhavy Singhal <debug.bhavysinghal@gmail.com>
2026-03-22 20:26:11 +0530

    semi-final

--- Frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.js'
-import './style.scss'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
